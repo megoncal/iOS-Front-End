1347c33f6d91b4069937530ae8bde07795118d49
Eduardo Goncalves <egoncalves@Eduardo-Goncalvess-MacBook-Pro.local>
2013-07-15 19:01:44 -0400

    Task List

--- Mtaxi/UserViewController.h
+++ Mtaxi/UserViewController.h
@@ -4,14 +4,33 @@
 lembretes
  
  
- 1 - falar com eduardo sobre o if que adicionei na classe marshaller... pode gerar efeito colateral..
+ 1 - falar com eduardo sobre o if que adicionei na classe marshaller... pode gerar efeito colateral.. (Marcos)
  
- 2 - continuam os erros apos o update do driver info e do passenger info... o update funciona, mas posteriormente o usuário e senha ficam invalidos.
+ 2 - continuam os erros apos o update do driver info e do passenger info... o update funciona, mas posteriormente o usuário e senha ficam invalidos - DONE
  
- 3 - precisamos adicionar o código para autenticar o usuário automaticamente quando receber o erro 403.
+ 3 - precisamos adicionar o código para autenticar o usuário automaticamente quando receber o erro 403 (Eduardo)
  
+ 4 - CarType Fix (Eduardo)
  
-
+ 5 - Mask for Passenger Phone (Marcos)
+ 
+ 6 - MyRides (Marcos)
+ 
+ 7 - Wait time indicator (Marcos)
+ 
+ 8 - Create Ride, Take Ride, Complete Ride Test (Eduardo)
+ 
+ 9 - As Passenger, Retrieve Driver assigned to the Ride (Marcos)
+ 
+ 10 - PickupAddress and Complement (Eduardo)
+ 
+ 11 - Can't Complete twice (Marcos)
+ 
+ 12 - Keyboard over in Passenger (Marcos)
+ 
+ 13 - Generic Audit Fields (version, lastUpdated, etc) (Eduardo)
+ 
+ 14 - Rationalize keyboard on UI (Eduardo)
  
  
  */
